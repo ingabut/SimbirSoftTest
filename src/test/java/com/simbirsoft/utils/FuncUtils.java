4de02ef15474a5ea1bbbd9f67c5d597a7018502a
i.butinskaya <i.butinskaya@goodt.me>
2023-03-01 13:36:45 +0500

    Task 3: fixed calc of fibonacci number

--- src/test/java/com/simbirsoft/utils/FuncUtils.java
+++ src/test/java/com/simbirsoft/utils/FuncUtils.java
@@ -2,11 +2,14 @@ package com.simbirsoft.utils;
 
 public class FuncUtils {
     public static int fibonacciCalc(int num) {
-        if (num <= 1) {
-            return num;
+        if (num == 1) {
+            return 0;
         }
-        int prev = 0, curr = 1;
-        for (int i = 0; i < num - 1; i++) {
+        if (num < 4) {
+            return 1;
+        }
+        int prev = 1, curr = 1;
+        for (int i = 4; i <= num ; i++) {
             int next = prev + curr;
             prev = curr;
             curr = next;
